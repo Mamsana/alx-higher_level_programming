f5e6e75c453b042e2500e91b0a13b5993960055c
Mamsana <alicemamsana@gmail.com>
2022-12-09 19:51:07 +0000

    a function that returns a tuple wih the length of a string and its first character

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,4 +6,5 @@ def max_integer(my_list=[]):
         max = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > max:
-        return max
+                max = my_list[i]
+            return max
